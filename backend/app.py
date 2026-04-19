f4c1babf14b6fb282ef420536a60a07f1570023c
deepakgurjar975275 <deepakgurjar975275@gmail.com>
2026-04-19 23:47:24 +0530

    Update app.py
    app.run(debug=True)   replace with  import os
    
    port = int(os.environ.get("PORT", 5000))
    
    app.run(host="0.0.0.0", port=port)

--- backend/app.py
+++ backend/app.py
@@ -92,7 +92,11 @@ def login():
     else:
         return jsonify({"status": "fail"}), 401
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+
+app.run(host="0.0.0.0", port=port)
 
     import requests
 
